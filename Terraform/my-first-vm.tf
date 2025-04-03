9644a2939dbcc566be937f8752b46a8c22dc33f5
AYUSH NITIN NAPHADE <98523356+ayushnaphade1008@users.noreply.github.com>
2025-04-03 06:23:43 +0000

    Demo 1

--- Terraform/my-first-vm.tf
+++ Terraform/my-first-vm.tf
@@ -1,3 +1,15 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
+    access_key = "AKIATTA3LV5CKVIO4BMP"
+    secret_key = "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"
+    # authenctication credentials are configured via aws cli
+}
+ 
+resource "aws_instance" "demo_instance" {
+    ami = "ami-08b5b3a93ed654d19"
+    instance_type = "t2.micro"
+ 
+    tags = {
+        Name = "my-ec2"
+    }
 }
