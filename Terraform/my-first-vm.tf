868eb6fbf18cc76a11268ab119506186772c8dab
AYUSH NITIN NAPHADE <98523356+ayushnaphade1008@users.noreply.github.com>
2025-04-07 09:00:27 +0000

    Latest commit

--- Terraform/my-first-vm.tf
+++ Terraform/my-first-vm.tf
@@ -2,12 +2,29 @@ provider "aws" {
     region = "us-east-1"
     # authenctication credentials are configured via aws cli
 }
- 
+
+resource "aws_eip" "elastic-ip" {
+  domain = "vpc"
+}
+
 resource "aws_instance" "demo_instance" {
     ami = "ami-08b5b3a93ed654d19"
     instance_type = "t2.micro"
  
     tags = {
-        Name = "my-ec2"
+        Name = "ayush"
     }
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "google"{
+    region = "europe-west1"
 }
